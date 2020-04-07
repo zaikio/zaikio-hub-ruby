783cd2f60ee79bdaf7eb60116864202c255fa794
Jalyna <jalyna.schroeder@gmail.com>
2020-04-07 10:07:35 +0200

    Add URI for organization membership

--- lib/zaikio/directory/membership.rb
+++ lib/zaikio/directory/membership.rb
@@ -1,6 +1,7 @@
 module Zaikio
   module Directory
     class Membership < Base
+      uri "organization/memberships(/:id)"
       include_root_in_json :membership
       has_one :person, uri: nil, class_name: "Zaikio::Directory::Person"
     end
