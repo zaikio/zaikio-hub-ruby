94df768b2021399e37bf739b112f0b70c7e4076b
Jalyna <jalyna.schroeder@gmail.com>
2021-02-22 18:09:32 +0100

    Add nested attributes for site address

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Changed
 
 * Only log Faraday req/response status lines, not headers
+* Added nested attributes for `address_attributes` in `Site`
 
 ## [0.3.0] - 2020-02-12
 
--- lib/zaikio/hub/site.rb
+++ lib/zaikio/hub/site.rb
@@ -7,6 +7,20 @@ module Zaikio
       # Attributes
       attributes :name, :headquarter, :created_at, :updated_at, :address,
                  :address_attributes
+
+      # Associations
+      has_one :address, uri: nil, class_name: "Zaikio::Hub::Address"
+
+      accepts_nested_attributes_for :address
+
+      # Callbacks
+      before_save :set_address_attributes
+
+      private
+
+      def set_address_attributes
+        attributes[:address_attributes] = address
+      end
     end
   end
 end
