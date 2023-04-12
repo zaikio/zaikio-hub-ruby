5cefc4d4787eac06930df474ceeb812668d6eda0
Jalyna <js@zaikio.com>
2023-04-12 15:32:00 +0200

    Add capabilities and translations (#141)
    * Add capabilities and translations
    
    * Add basic translations

--- lib/zaikio/hub/machine.rb
+++ lib/zaikio/hub/machine.rb
@@ -7,7 +7,7 @@ module Zaikio
       include_root_in_json :machine
 
       # Attributes
-      attributes :name, :updated_at, :created_at, :site_id, :kind,
+      attributes :name, :updated_at, :created_at, :site_id, :kind, :capabilities,
                  :manufacturer_id, :serial_number, :manufacturer, :owner_id
     end
   end
